e13d5eeaaafb8bb1b12a1d87654138da91c930b6
lwertani <Salah.aziza@121212>
2023-12-18 12:07:04 +0100

    first commit

---
+++ src/main/java/struts_example/TestAction.java
@@ -0,0 +1,24 @@
+package struts_example;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class TestAction extends ActionSupport {
+
+	public String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String execute() throws Exception {
+		name = "TEST";
+        ActionContext.getContext().put("name", "Hello from context!");      
+		return SUCCESS;
+	}
+}
